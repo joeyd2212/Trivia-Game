924c2afe163a7bf2c70a329cdb4773123005a273
DEE ALLEN <JOEYPBA09@GMAIL.COM>
2019-07-17 19:19:46 -0400

    added variables and init function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,96 @@
+let startButton;
+let triviadiv;
+let timerDiv;
+let questionDiv;
+let resultsDiv;
+let finalScoreDiv;
+let answerTextBox;
+
+
+//variables
+//indicate if the game is in progress
+
+var inProgress;
+
+
+//time limit in seconds
+var timeLimit;
+
+
+//current question number
+var questionNum;
+
+
+//declares player's score
+
+var score;
+
+//list of trivia questions
+var questions = [
+{
+	question: "What is 1 + 1? ",
+	correctAnswer: "2"
+},
+{ 
+	
+	question: "question ",
+	correctAnswer: "answer"
+},
+
+{ 
+	
+	question: "question ",
+	correctAnswer: "answer"
+},
+{ 
+	
+	question: "question ",
+	correctAnswer: "answer"
+},
+{ 
+	
+	question: "question ",
+	correctAnswer: "answer"
+}
+
+
+];
+
+
+
+
+//initialize the DOM elements
+
+function init() {
+	startButton= document.getElementById("start-button");
+	triviadiv = document.getElementById("trivia");
+	timerDiv = document.getElementById("timer");
+	questionDiv = document.getElementById("question");
+	answerTextBox = document.getElementById("answer");
+	resultsDiv = document.getElementById("results");
+	finalScoreDiv = document.getElementById("final-score");
+}
+
+
+
+
+
+//start the game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
